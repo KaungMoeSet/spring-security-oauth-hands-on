8a05412e891b4c440bfb3f8e0fad7ad9416c8bad
KaungMoeSet <kaungmoeset@abank.com.mm>
2026-01-29 16:20:05 +0630

    initial commit

---
+++ src/main/java/org/example/auth/springsecurityoauthhandson/SpringSecurityOauthHandsOnApplication.java
@@ -0,0 +1,13 @@
+package org.example.auth.springsecurityoauthhandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthHandsOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauthHandsOnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/auth/springsecurityoauthhandson/SpringSecurityOauthHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.auth.springsecurityoauthhandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthHandsOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
